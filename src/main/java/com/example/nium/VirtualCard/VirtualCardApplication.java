e6a443d882e9ba2e55367b8b08a2c84f9a1c8b0d
ReubenXuereb <56310716+ReubenXuereb@users.noreply.github.com>
2025-10-24 14:43:46 +0200

    Uploaded base project

---
+++ src/main/java/com/example/nium/VirtualCard/VirtualCardApplication.java
@@ -0,0 +1,13 @@
+package com.example.nium.VirtualCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nium/VirtualCard/VirtualCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nium.VirtualCard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
